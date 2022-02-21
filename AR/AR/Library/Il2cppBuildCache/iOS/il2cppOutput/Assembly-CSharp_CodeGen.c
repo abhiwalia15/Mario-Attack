7647c6e278127d0e249c3da439aaf7b13414d816
abhiwalia15 <waliamrinal@gmail.com>
2022-02-20 21:47:50 -0500

    commit changes

---
+++ AR/AR/Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,275 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CameraScript::Start()
+extern void CameraScript_Start_mFA9D3EAD61A387624F2D8E2AB938D32CB43C2AAD (void);
+// 0x00000002 System.Void CameraScript::fire()
+extern void CameraScript_fire_m736B0DC40916CA146DBE9BC3F56924167E57E595 (void);
+// 0x00000003 System.Void CameraScript::Update()
+extern void CameraScript_Update_mFD3343EC9B5F59B8D44E7151FA49C7CE88B1936A (void);
+// 0x00000004 System.Void CameraScript::.ctor()
+extern void CameraScript__ctor_m2F555CA91AB185743DD59C5C093A7F6EE339B863 (void);
+// 0x00000005 System.Void MenuSceneLoader::Awake()
+extern void MenuSceneLoader_Awake_m18B4E09AAFDC9578469D066C50A8AC0054AA34AC (void);
+// 0x00000006 System.Void MenuSceneLoader::.ctor()
+extern void MenuSceneLoader__ctor_m27FD45CA6C6C8B579D2FA4EEDABBA35F2C7EF6BC (void);
+// 0x00000007 System.Void PauseMenu::Awake()
+extern void PauseMenu_Awake_mB04BCBDA84AEC654D1976EC3DF61A0CF68D2C86C (void);
+// 0x00000008 System.Void PauseMenu::MenuOn()
+extern void PauseMenu_MenuOn_m8973DC956DD5235381EE4ACB4A182AA5BF0EF2EA (void);
+// 0x00000009 System.Void PauseMenu::MenuOff()
+extern void PauseMenu_MenuOff_mCD1AF41F916B0C02D2FD6F82377DBEAFF7C30720 (void);
+// 0x0000000A System.Void PauseMenu::OnMenuStatusChange()
+extern void PauseMenu_OnMenuStatusChange_mD1E8BD9B9CCB274A83FFF0FE2E06E6ABD0361B4A (void);
+// 0x0000000B System.Void PauseMenu::.ctor()
+extern void PauseMenu__ctor_mA1A281F3359C234E5CF24FFEAC20C12C48D69018 (void);
+// 0x0000000C System.Void SceneAndURLLoader::Awake()
+extern void SceneAndURLLoader_Awake_m8F276157A2A5FA943EF7918D6CCDB81273317E23 (void);
+// 0x0000000D System.Void SceneAndURLLoader::SceneLoad(System.String)
+extern void SceneAndURLLoader_SceneLoad_m2B09BD48F419F49A6BD461DBC7B2290EC8632B06 (void);
+// 0x0000000E System.Void SceneAndURLLoader::LoadURL(System.String)
+extern void SceneAndURLLoader_LoadURL_m47E3E286E80F2D5B3E6A164C32F7E1B473532AE2 (void);
+// 0x0000000F System.Void SceneAndURLLoader::.ctor()
+extern void SceneAndURLLoader__ctor_m6DEE574FADF9E3E894594690CB2755F69D5D4BE5 (void);
+// 0x00000010 System.Void CameraSwitch::OnEnable()
+extern void CameraSwitch_OnEnable_mD422991EDD9D880928644EE1BC4E557EE644679C (void);
+// 0x00000011 System.Void CameraSwitch::NextCamera()
+extern void CameraSwitch_NextCamera_m38AB4521C129032FA1DA5154E09D36D0FE2DB257 (void);
+// 0x00000012 System.Void CameraSwitch::.ctor()
+extern void CameraSwitch__ctor_m550FCA9B0C24BBBC8BDBCAAAAA7BBF26312399FE (void);
+// 0x00000013 System.Void LevelReset::OnPointerClick(UnityEngine.EventSystems.PointerEventData)
+extern void LevelReset_OnPointerClick_m9625C8235343CB1DE6B089AD6F2D5ACF738072A4 (void);
+// 0x00000014 System.Void LevelReset::.ctor()
+extern void LevelReset__ctor_mFD2BB9AF1A9D7E795DDAAE2F33C1F0EB1FB31F07 (void);
+// 0x00000015 System.Void collisionScript::Start()
+extern void collisionScript_Start_mD01C233D30A1E8DCF6AD4904CF7D429FBE5B5230 (void);
+// 0x00000016 System.Void collisionScript::Update()
+extern void collisionScript_Update_m0CBB0088ECEF78FA8B4D1FE3017BBC7ED0B13E54 (void);
+// 0x00000017 System.Void collisionScript::OnTriggerEnter(UnityEngine.Collider)
+extern void collisionScript_OnTriggerEnter_m7BFA6800CCAC740C7D8EABCF0D5755B80165D61E (void);
+// 0x00000018 System.Void collisionScript::.ctor()
+extern void collisionScript__ctor_m54062A804DE2ACEA2C1071D6E27BBD57891596AA (void);
+// 0x00000019 System.Void countdownTimer::Start()
+extern void countdownTimer_Start_m4319C4E609472066FAD5D509CC5B188B36322D1D (void);
+// 0x0000001A System.Void countdownTimer::Update()
+extern void countdownTimer_Update_m5A500A8A679CF7A4F0F40519750F92B0531E827F (void);
+// 0x0000001B System.Void countdownTimer::.ctor()
+extern void countdownTimer__ctor_m26AEB9987462277CCC6CEDF57FEE7D36615929C6 (void);
+// 0x0000001C System.Void enemyScript::Start()
+extern void enemyScript_Start_mA031B42A54C03EC1D97301EEC79DE254519271F8 (void);
+// 0x0000001D System.Void enemyScript::Update()
+extern void enemyScript_Update_m43AAC3B918BF6DC8160DE0B624B90AFFEA3361BF (void);
+// 0x0000001E System.Collections.IEnumerator enemyScript::Move()
+extern void enemyScript_Move_mFFC2744AD4E05E611DD0164CD75C8024BEDA448B (void);
+// 0x0000001F System.Void enemyScript::.ctor()
+extern void enemyScript__ctor_m005BF5D19D66C34D0784565FC12349A72D391E0F (void);
+// 0x00000020 System.Void enemyScript/<Move>d__2::.ctor(System.Int32)
+extern void U3CMoveU3Ed__2__ctor_m10B8BE9510ED3C100CF3EE557DEFEDA14EBE4A27 (void);
+// 0x00000021 System.Void enemyScript/<Move>d__2::System.IDisposable.Dispose()
+extern void U3CMoveU3Ed__2_System_IDisposable_Dispose_m9548148DC453FBB5BA1D1CD642ED3EE39608CC87 (void);
+// 0x00000022 System.Boolean enemyScript/<Move>d__2::MoveNext()
+extern void U3CMoveU3Ed__2_MoveNext_mDCAC3BEEAF0019C5CDE1448E5862122FFD73005B (void);
+// 0x00000023 System.Object enemyScript/<Move>d__2::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CMoveU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB137B05D754CB940A1D66B986934655C6AFC9E9E (void);
+// 0x00000024 System.Void enemyScript/<Move>d__2::System.Collections.IEnumerator.Reset()
+extern void U3CMoveU3Ed__2_System_Collections_IEnumerator_Reset_m5D567B1C00F52C89C4FEB3E315FA8FC1A710AD37 (void);
+// 0x00000025 System.Object enemyScript/<Move>d__2::System.Collections.IEnumerator.get_Current()
+extern void U3CMoveU3Ed__2_System_Collections_IEnumerator_get_Current_m56C02DE68663D26EC9D5A63A31D33ABE905CF822 (void);
+// 0x00000026 System.Void webCamScript::Start()
+extern void webCamScript_Start_m3F4A69CD99961E7CF238AE6D2C53E5F53B139F1F (void);
+// 0x00000027 System.Void webCamScript::OnButtonDown()
+extern void webCamScript_OnButtonDown_m33452C36FCA05AF1A3F53DA7291404925F191802 (void);
+// 0x00000028 System.Void webCamScript::Update()
+extern void webCamScript_Update_mBEC02005274B4DA2D484FE73E316AC007DFAAD19 (void);
+// 0x00000029 System.Void webCamScript::.ctor()
+extern void webCamScript__ctor_m8875A7829D09FCF52310AC484CF575525D2F5AD3 (void);
+// 0x0000002A System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::Awake()
+extern void ParticleSceneControls_Awake_m3268C5B5B81D4EE59C85C27AED5CA3956C284DA4 (void);
+// 0x0000002B System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::OnDisable()
+extern void ParticleSceneControls_OnDisable_mEB5EAF7BC1928EBEFD83E651459327948255D811 (void);
+// 0x0000002C System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::Previous()
+extern void ParticleSceneControls_Previous_mC2FE564898A6019238C5611E63B5EC3581CEAA63 (void);
+// 0x0000002D System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::Next()
+extern void ParticleSceneControls_Next_m4AB6C18CBE0148C4D354FDCAD5EE45D472577B8E (void);
+// 0x0000002E System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::Update()
+extern void ParticleSceneControls_Update_m7B5894AE70D3733C901BFFF529195AC1467D632B (void);
+// 0x0000002F System.Boolean UnityStandardAssets.SceneUtils.ParticleSceneControls::CheckForGuiCollision()
+extern void ParticleSceneControls_CheckForGuiCollision_mB8EDC5E34CE1B59986991852009A011D9514A8FB (void);
+// 0x00000030 System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::Select(System.Int32)
+extern void ParticleSceneControls_Select_m123BA8121C7C5C454EF649A13C724F2622B4517A (void);
+// 0x00000031 System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::.ctor()
+extern void ParticleSceneControls__ctor_mFE76B488C636F213B77193EDFF38F514A3186631 (void);
+// 0x00000032 System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls::.cctor()
+extern void ParticleSceneControls__cctor_mBA868BA9BC2400AD1AB420B3D0AB42D863358338 (void);
+// 0x00000033 System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls/DemoParticleSystem::.ctor()
+extern void DemoParticleSystem__ctor_mA046B73C934441755CA5D57991D7C453645AD8CE (void);
+// 0x00000034 System.Void UnityStandardAssets.SceneUtils.ParticleSceneControls/DemoParticleSystemList::.ctor()
+extern void DemoParticleSystemList__ctor_mAFBB16272EB2E695826211DD030553401DF6E83B (void);
+// 0x00000035 System.Void UnityStandardAssets.SceneUtils.PlaceTargetWithMouse::Update()
+extern void PlaceTargetWithMouse_Update_m28E68FE68FA0185C0D20A6001F4C262868348BE3 (void);
+// 0x00000036 System.Void UnityStandardAssets.SceneUtils.PlaceTargetWithMouse::.ctor()
+extern void PlaceTargetWithMouse__ctor_m3582FA7DC3CF8198244F9EB014FE12AC97985FE7 (void);
+// 0x00000037 System.Void UnityStandardAssets.SceneUtils.SlowMoButton::Start()
+extern void SlowMoButton_Start_m8F334286C4BEE772EAA4842F431E01BB4929A04C (void);
+// 0x00000038 System.Void UnityStandardAssets.SceneUtils.SlowMoButton::OnDestroy()
+extern void SlowMoButton_OnDestroy_mE8D51330ED641B12795E984B6F0ECCB00536DBDB (void);
+// 0x00000039 System.Void UnityStandardAssets.SceneUtils.SlowMoButton::ChangeSpeed()
+extern void SlowMoButton_ChangeSpeed_m0C19576C039AA7BBBC478C87A6C964376BA58EAB (void);
+// 0x0000003A System.Void UnityStandardAssets.SceneUtils.SlowMoButton::.ctor()
+extern void SlowMoButton__ctor_m993A85A43C7462445119B63636FD65A6F8C0F6BF (void);
+static Il2CppMethodPointer s_methodPointers[58] = 
+{
+	CameraScript_Start_mFA9D3EAD61A387624F2D8E2AB938D32CB43C2AAD,
+	CameraScript_fire_m736B0DC40916CA146DBE9BC3F56924167E57E595,
+	CameraScript_Update_mFD3343EC9B5F59B8D44E7151FA49C7CE88B1936A,
+	CameraScript__ctor_m2F555CA91AB185743DD59C5C093A7F6EE339B863,
+	MenuSceneLoader_Awake_m18B4E09AAFDC9578469D066C50A8AC0054AA34AC,
+	MenuSceneLoader__ctor_m27FD45CA6C6C8B579D2FA4EEDABBA35F2C7EF6BC,
+	PauseMenu_Awake_mB04BCBDA84AEC654D1976EC3DF61A0CF68D2C86C,
+	PauseMenu_MenuOn_m8973DC956DD5235381EE4ACB4A182AA5BF0EF2EA,
+	PauseMenu_MenuOff_mCD1AF41F916B0C02D2FD6F82377DBEAFF7C30720,
+	PauseMenu_OnMenuStatusChange_mD1E8BD9B9CCB274A83FFF0FE2E06E6ABD0361B4A,
+	PauseMenu__ctor_mA1A281F3359C234E5CF24FFEAC20C12C48D69018,
+	SceneAndURLLoader_Awake_m8F276157A2A5FA943EF7918D6CCDB81273317E23,
+	SceneAndURLLoader_SceneLoad_m2B09BD48F419F49A6BD461DBC7B2290EC8632B06,
+	SceneAndURLLoader_LoadURL_m47E3E286E80F2D5B3E6A164C32F7E1B473532AE2,
+	SceneAndURLLoader__ctor_m6DEE574FADF9E3E894594690CB2755F69D5D4BE5,
+	CameraSwitch_OnEnable_mD422991EDD9D880928644EE1BC4E557EE644679C,
+	CameraSwitch_NextCamera_m38AB4521C129032FA1DA5154E09D36D0FE2DB257,
+	CameraSwitch__ctor_m550FCA9B0C24BBBC8BDBCAAAAA7BBF26312399FE,
+	LevelReset_OnPointerClick_m9625C8235343CB1DE6B089AD6F2D5ACF738072A4,
+	LevelReset__ctor_mFD2BB9AF1A9D7E795DDAAE2F33C1F0EB1FB31F07,
+	collisionScript_Start_mD01C233D30A1E8DCF6AD4904CF7D429FBE5B5230,
+	collisionScript_Update_m0CBB0088ECEF78FA8B4D1FE3017BBC7ED0B13E54,
+	collisionScript_OnTriggerEnter_m7BFA6800CCAC740C7D8EABCF0D5755B80165D61E,
+	collisionScript__ctor_m54062A804DE2ACEA2C1071D6E27BBD57891596AA,
+	countdownTimer_Start_m4319C4E609472066FAD5D509CC5B188B36322D1D,
+	countdownTimer_Update_m5A500A8A679CF7A4F0F40519750F92B0531E827F,
+	countdownTimer__ctor_m26AEB9987462277CCC6CEDF57FEE7D36615929C6,
+	enemyScript_Start_mA031B42A54C03EC1D97301EEC79DE254519271F8,
+	enemyScript_Update_m43AAC3B918BF6DC8160DE0B624B90AFFEA3361BF,
+	enemyScript_Move_mFFC2744AD4E05E611DD0164CD75C8024BEDA448B,
+	enemyScript__ctor_m005BF5D19D66C34D0784565FC12349A72D391E0F,
+	U3CMoveU3Ed__2__ctor_m10B8BE9510ED3C100CF3EE557DEFEDA14EBE4A27,
+	U3CMoveU3Ed__2_System_IDisposable_Dispose_m9548148DC453FBB5BA1D1CD642ED3EE39608CC87,
+	U3CMoveU3Ed__2_MoveNext_mDCAC3BEEAF0019C5CDE1448E5862122FFD73005B,
+	U3CMoveU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB137B05D754CB940A1D66B986934655C6AFC9E9E,
+	U3CMoveU3Ed__2_System_Collections_IEnumerator_Reset_m5D567B1C00F52C89C4FEB3E315FA8FC1A710AD37,
+	U3CMoveU3Ed__2_System_Collections_IEnumerator_get_Current_m56C02DE68663D26EC9D5A63A31D33ABE905CF822,
+	webCamScript_Start_m3F4A69CD99961E7CF238AE6D2C53E5F53B139F1F,
+	webCamScript_OnButtonDown_m33452C36FCA05AF1A3F53DA7291404925F191802,
+	webCamScript_Update_mBEC02005274B4DA2D484FE73E316AC007DFAAD19,
+	webCamScript__ctor_m8875A7829D09FCF52310AC484CF575525D2F5AD3,
+	ParticleSceneControls_Awake_m3268C5B5B81D4EE59C85C27AED5CA3956C284DA4,
+	ParticleSceneControls_OnDisable_mEB5EAF7BC1928EBEFD83E651459327948255D811,
+	ParticleSceneControls_Previous_mC2FE564898A6019238C5611E63B5EC3581CEAA63,
+	ParticleSceneControls_Next_m4AB6C18CBE0148C4D354FDCAD5EE45D472577B8E,
+	ParticleSceneControls_Update_m7B5894AE70D3733C901BFFF529195AC1467D632B,
+	ParticleSceneControls_CheckForGuiCollision_mB8EDC5E34CE1B59986991852009A011D9514A8FB,
+	ParticleSceneControls_Select_m123BA8121C7C5C454EF649A13C724F2622B4517A,
+	ParticleSceneControls__ctor_mFE76B488C636F213B77193EDFF38F514A3186631,
+	ParticleSceneControls__cctor_mBA868BA9BC2400AD1AB420B3D0AB42D863358338,
+	DemoParticleSystem__ctor_mA046B73C934441755CA5D57991D7C453645AD8CE,
+	DemoParticleSystemList__ctor_mAFBB16272EB2E695826211DD030553401DF6E83B,
+	PlaceTargetWithMouse_Update_m28E68FE68FA0185C0D20A6001F4C262868348BE3,
+	PlaceTargetWithMouse__ctor_m3582FA7DC3CF8198244F9EB014FE12AC97985FE7,
+	SlowMoButton_Start_m8F334286C4BEE772EAA4842F431E01BB4929A04C,
+	SlowMoButton_OnDestroy_mE8D51330ED641B12795E984B6F0ECCB00536DBDB,
+	SlowMoButton_ChangeSpeed_m0C19576C039AA7BBBC478C87A6C964376BA58EAB,
+	SlowMoButton__ctor_m993A85A43C7462445119B63636FD65A6F8C0F6BF,
+};
+static const int32_t s_InvokerIndices[58] = 
+{
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1044,
+	1044,
+	1222,
+	1222,
+	1222,
+	1222,
+	1044,
+	1222,
+	1222,
+	1222,
+	1044,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1188,
+	1222,
+	1034,
+	1222,
+	1209,
+	1188,
+	1222,
+	1188,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1209,
+	1034,
+	1222,
+	2080,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+	1222,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	58,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
